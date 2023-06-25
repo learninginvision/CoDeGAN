acdaa1195df86b08775ff0f281fd6a21833cd53a
ZhaoJW <2574298210@qq.com>
2023-06-25 20:04:15 +0800

    fix FID_pytorch version

--- test/test_FID_pt.py
+++ test/test_FID_pt.py
@@ -1,12 +1,13 @@
-from cleanfid import fid
 import yaml
 import torch
-from utils.util import sample_noise, make_batches
 import os
+import sys
+sys.path.append("..")
+from utils.util import sample_noise, make_batches
 from solvers.solver import get_solvers
 from torchvision.utils import save_image
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
-
+from cleanfid import fid
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--model', type=str, required= True, 
                     help='Path of the model')
@@ -14,7 +15,7 @@ parser.add_argument('--config', type=str, required= True,
                     help='Path of the config')
 
 # download featrures
-# cleanfid.features.get_reference_statistics(name, res, mode="clean", model_name="inception_v3", seed=0, split="test", metric="FID") 
+# cleanfid.features.get_reference_statistics(name, res, mode="legacy_pytorch", model_name="inception_v3", seed=0, split="train", metric="FID") 
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -25,7 +26,10 @@ if __name__ == "__main__":
     solver.G.to(solver.device)
     solver.G.eval()
 
-    z = sample_noise(batch_size=50000, device=solver.device)[0]
+    if not os.path.exists('./tmp'):
+        os.mkdir('./tmp')
+
+    z = sample_noise(batch_size=50000, zn_dim=solver.zn_dim, zc_dim=solver.zc_dim, device=solver.device)[0]
     batches = make_batches(50000, 25)
     fake_images = []
     idx = 0
@@ -33,10 +37,14 @@ if __name__ == "__main__":
         noise_batch = z[batch_start:batch_end].to(solver.device)
         out = solver.G(noise_batch).detach().cpu()
         for i in range(out.shape[0]):
-            save_image(out[0], os.path.join('./test/tmp', '{}.png'.format(i)),normalize = True)
+            save_image(out[0], os.path.join('./tmp', '{}.png'.format(idx)),normalize = True)
             idx +=1
     del solver
     torch.cuda.empty_cache()
+    print('Generate images finish!')
     # FID
-    score = fid.compute_fid('./test/tmp', dataset_name=config['dataset'], dataset_res=32, dataset_split="train")
-    
+    score = fid.compute_fid('./tmp', dataset_name=config['dataset'], dataset_res=32, dataset_split="train", mode="legacy_pytorch")
+    print(f'FID={score}')
+    exp_path = os.path.abspath(os.path.join(model_dir, "../.."))
+    with open(file=os.path.join(exp_path, 'fid.txt'), mode='a') as f:
+        f.write(f'FID={score}, model file= {model_dir} \n')
